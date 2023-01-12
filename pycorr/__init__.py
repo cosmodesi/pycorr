568e7afb57abae50b064dd5a11aa5edfaa9e0b43
Arnaud De-Mattia <adematti@dphppcz231.extra.cea.fr>
2023-01-12 11:09:26 +0100

    import project_to_wedges at the package level

--- pycorr/__init__.py
+++ pycorr/__init__.py
@@ -4,7 +4,7 @@ from ._version import __version__
 from .twopoint_counter import TwoPointCounter, AnalyticTwoPointCounter
 from .twopoint_jackknife import BoxSubsampler, KMeansSubsampler, JackknifeTwoPointCounter, JackknifeTwoPointEstimator
 from .twopoint_estimator import TwoPointEstimator, NaturalTwoPointEstimator, LandySzalayTwoPointEstimator,\
-                                project_to_poles, project_to_wp
+                                project_to_poles, project_to_wedges, project_to_wp
 from .twopoint_estimator import project_to_multipoles  # for backward-compatibility
 from .correlation_function import TwoPointCorrelationFunction
 from .utils import setup_logging
